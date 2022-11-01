5870279038e80ebf39b334c8e0a1191ee428ceb1
Leonardo Conroy <leeleonard936@gmail.com>
2022-11-01 12:59:13 -0400

    bootstrapped and navbarrred

--- front-end-react/report-front/src/App.js
+++ front-end-react/report-front/src/App.js
@@ -1,9 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    
     <div className="App">
+      <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+            <Nav.Link href="#link">About Us</Nav.Link>
+            <Nav.Link href="#link">Report</Nav.Link>
+            <NavDropdown title="More Resources" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Hotlines</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Crisis Counseling</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">Know your Options</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
