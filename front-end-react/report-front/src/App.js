dbfa7ec9b8c00ab2b7bac216de43c559d5f647f1
Leonardo Conroy <leeleonard936@gmail.com>
2022-11-04 14:05:43 -0400

    mapmapmapmap + marker

--- front-end-react/report-front/src/App.js
+++ front-end-react/report-front/src/App.js
@@ -11,16 +11,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 //GOOGLE MAP API DEPENDENCIES V IMPORTANT PLZ DO NOT FUCK WITH THIS THANK YOU :)
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker} from '@react-google-maps/api';
+
 const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
+   width: '100%',
+   height: '500px'
+ };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523
+  lat: 38.8,
+  lng: -76.7
 };
+
+const position = {
+  lat: 39.3925,
+  lng: -76.6126
+};
+const onLoad = marker => {
+  console.log('marker: ', marker)
+}
+
 function App() {
   const [data, setData] = React.useState(null);
 
@@ -48,24 +58,30 @@ function App() {
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.3">Know your Options</NavDropdown.Item>
             </NavDropdown>
+            <img src={logo} className="App-logo" alt="logo" />
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
       <header className="App-header">
       <LoadScript
-        googleMapsApiKey="API KEY HERE"
+        googleMapsApiKey="INSERT KEY"
       >
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
-          zoom={10}
+          zoom={8.125}
+          disableDefaultUI='true'
         >
           { /* Child components, such as markers, info windows, etc. */ }
           <></>
+          <Marker
+      onLoad={onLoad}
+      position={position}
+      />
         </GoogleMap>
       </LoadScript>
-       <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           {!data ? "Loading..." : data}
         </p>
