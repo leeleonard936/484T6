216d454223b391a652e3420ef7abe57ab0e423fa
Robert  Schattner <robertschattner@Roberts-MacBook-Pro.local>
2022-11-08 15:30:11 -0500

    trying to not fuck up my branch, express endpoind sends gets data from mongo

--- back-end-api/report-api/report-api.js
+++ back-end-api/report-api/report-api.js
@@ -1,15 +1,30 @@
+//best resource for linking RESTful APIs and MongoDB
+//https://www.mongodb.com/languages/express-mongodb-rest-api-tutorial
+
 const express = require("express");
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const PORT = process.env.PORT || 3001;
-
+const uri = "mongodb+srv://group6:horseNeck2023@reportdata.5sr0hj8.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const app = express();
 
+//reading all documents from ReportData Collection
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+  client.connect(err => {
+    if(err){console.log("Problems Connecting to database")}
+    const collection = client.db("Group6").collection("ReportData");
+    
+    collection
+    .find({})
+    .toArray((err, result) => {
+        if(err){throw err;}
+        //console.log(res.json(result));
+        client.close();
+    });
   });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-//hi there
