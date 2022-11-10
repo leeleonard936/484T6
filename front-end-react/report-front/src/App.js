b49f5bb8847bde1d02728231848f2f4f0ce99188
Leonardo Conroy <leeleonard936@gmail.com>
2022-11-10 18:30:34 -0500

    containers for pages

--- front-end-react/report-front/src/App.js
+++ front-end-react/report-front/src/App.js
@@ -1,30 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from "react";
-import {useState} from "react";
-
 //bootstrap dependencies
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import {compose, withProps, withStateHandlers} from 'recompose';
-
-//GOOGLE MAP API DEPENDENCIES V IMPORTANT PLZ DO NOT FUCK WITH THIS THANK YOU :)
-import {GoogleMap, LoadScript, Marker, InfoWindow} from '@react-google-maps/api';
-import {withScriptjs, withGoogleMap} from '@react-google-maps/api'
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-import {markers} from './Data/markers'
+//import {markers} from './Data/markers'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 
 import AboutUs from './pages/AboutUs'
 import MapHome from './pages'
+import LoginPage from './pages/LoginPage'
+import MakeReport from './pages/MakeReport'
 
 
 function App() {
@@ -40,6 +29,8 @@ function App() {
       <Routes>
         <Route exact path='/' element={<MapHome />} />
         <Route exact path='/AboutUs' element={<AboutUs />} />
+        <Route exact path='/LoginPage' element={<LoginPage />} />
+        <Route exact path='/MakeReport' element={<MakeReport />} />
     </Routes>
     <Footer/>
     </header>
