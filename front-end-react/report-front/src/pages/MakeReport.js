a97057f5916f8ad689aec129d1f2667158c11a40
Leonardo Conroy <leeleonard936@gmail.com>
2022-11-11 17:46:21 -0500

    start on form

--- front-end-react/report-front/src/pages/MakeReport.js
+++ front-end-react/report-front/src/pages/MakeReport.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import {markers} from './../Data/markers'
 
 const AboutUs = () => {
 return (
@@ -10,21 +11,35 @@ return (
             <h1>
 		        Make A Report
 	        </h1>
+			<p className = "verify">
+				By filling out this form you verify that you, an anonymous individual, are a victim of sexual assault on a college campus.
+			</p>
 			<Form>
 				<div>
+					<p>What school do you or your assaulter attend?</p>
+					<Form.Select label="schoolName">
+						<option></option>
+						{markers.map(({ id, Name}) => (
+      						<option value={id}>{Name}</option>
+						))}
+    				</Form.Select>
+				</div>
+				<br/>
+				<div>
+					<p>Did the incident occur on the campus of that institution? </p>
           			<Form.Check
-            inline
-            label="1"
-            name="group1"
-            type="radio"
-            id="oncampus-yes"
+            			inline
+            			label="Yes"
+            			name="oncampus"
+            			type="radio"
+            			id="oncampus-yes"
           			/>
           			<Form.Check
-            inline
-            label="1"
-            name="group1"
-            type="radio"
-            id="oncampus-no"
+            			inline
+            			label="No"
+            			name="oncampus"
+            			type="radio"
+            			id="oncampus-no"
           			/>
 		  		</div>
 			</Form>
