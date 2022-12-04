faac1c2c7488a2c6bf7693df183ccf29d018c7c2
Robert Schattner <robertvschattner@gmail.com>
2022-12-04 03:37:42 -0500

    functions are working man

---
+++ back-end-api/report-api/other.js
@@ -0,0 +1,12 @@
+//file to test exporting modules:
+
+var reportAPI = require('./report-api.js');
+
+//var x = reportAPI.startServer();
+//var y = reportAPI.getAllReports();
+
+reportAPI.getNumberOfReports("Towson University", function (err, count) {
+    if (!err) {
+      console.log(count);
+    }
+});
--- back-end-api/report-api/report-api.js
+++ back-end-api/report-api/report-api.js
@@ -8,23 +8,83 @@ const uri = "mongodb+srv://group6:horseNeck2023@reportdata.5sr0hj8.mongodb.net/?
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const app = express();
 
-//reading all documents from ReportData Collection
-app.get("/api", (req, res) => {
-  client.connect(err => {
-    if(err){console.log("Problems Connecting to database")}
-    const collection = client.db("Group6").collection("ReportData");
-    
-    collection
-    .find({})
-    .toArray((err, result) => {
-        if(err){throw err;}
-        res.json(result);
-        client.close();
-    });
-  });
-});
-//testing changes
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+//starts the server
+  //cant tell if I need this or not...
+  //is it just so I can hit the endpoint?
+function startServer() {
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  })
+
+};
+
+//reading all documents from ReportData Collection when website hits 
+// "/api" which will be changed to "/researcher"
+function getAllReports() {
+
+  app.get("/api", (req, res) => {
+    client.connect(err => {
+      if (err) { console.log("Problems Connecting to database") }
+      const collection = client.db("Group6").collection("ReportData");
+
+      collection
+        .find({})
+        .toArray((err, result) => {
+          if (err) { throw err; }
+          res.json(result);
+          client.close();
+        });
+    });
+  })
+
+};
+
+//returns the number of times a school name shows up in the report collection
+function getNumberOfReports(schoolName, callback) {
+
+  client.connect(err => {
+
+    if (err) { console.log("Problems Connecting to database") }
+    const collection = client.db("Group6").collection("ReportData");
+
+    collection
+      .find({school: schoolName})
+      .toArray((err, result) => {
+        if (err) { return callback(err); }
+      
+        //console.log("From inside async " + result.length); //finds proper length can i return this value?
+        
+        client.close();  
+
+        callback(null, result.length);
+      });
+
+  });
+
+  
+};
+
+
+
+
+
+
+
+//call each function to test
+//if these test functions arent commented out, then they will execute when this file is 
+//required/imported else where
+//console.log("I will print from inside report-api.js when this file is imported");
+//startServer();
+
+//useage of getNumberOfReports():
+// getNumberOfReports("Towson University", function (err, count) {
+//   if (!err) {
+//     console.log(count);
+//   }
+// });
+
+
+//now how do i call this function from another file?
+module.exports = {startServer, getAllReports, getNumberOfReports};
