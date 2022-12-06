ab282d9357a0826ac14a1b089e5e0089c6f9d977
Leonardo Conroy <leeleonard936@gmail.com>
2022-12-06 18:35:37 -0500

    fetch

--- front-end-react/report-front/src/components/Footer/index.js
+++ front-end-react/report-front/src/components/Footer/index.js
@@ -9,11 +9,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-
+const url = "https://us-central1-host-484-backend.cloudfunctions.net/app"
 const Footer = () => {
     const [data, setData] = React.useState(null);
     React.useEffect(() => {
-        fetch("/reportData")
+        fetch("https://us-central1-host-484-backend.cloudfunctions.net/app/reportData")
           .then((res) => res.json())
           .then((data) => setData(data));
       }, []);
